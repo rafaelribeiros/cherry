183a7c23c9c87040728465e179adce72e6b72c2e
DESKTOP-D8V1DPN\Rafael <rafael.ribeiro_s@hotmail.com>
2018-04-08 17:17:16 -0300

    Instalação do package react-native-fast-image

--- android/app/src/main/java/com/cherry/MainApplication.java
+++ android/app/src/main/java/com/cherry/MainApplication.java
@@ -3,6 +3,7 @@ package com.cherry;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.dylanvann.fastimage.FastImageViewPackage;
 import com.github.droibit.android.reactnative.customtabs.CustomTabsPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.facebook.reactnative.androidsdk.FBSDKPackage;
@@ -35,6 +36,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FastImageViewPackage(),
             new CustomTabsPackage(),
             new LinearGradientPackage(),
             new FBSDKPackage(mCallbackManager),
