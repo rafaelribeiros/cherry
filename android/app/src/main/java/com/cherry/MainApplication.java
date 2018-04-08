10014ef628bd446100ce498dcf7f2325db7446dc
DESKTOP-D8V1DPN\Rafael <rafael.ribeiro_s@hotmail.com>
2018-04-08 15:12:19 -0300

    Instalação do package react-native-fbsdk

--- android/app/src/main/java/com/cherry/MainApplication.java
+++ android/app/src/main/java/com/cherry/MainApplication.java
@@ -3,6 +3,8 @@ package com.cherry;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.facebook.CallbackManager;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -15,6 +17,12 @@ import java.util.List;
 
 public class MainApplication extends Application implements ReactApplication {
 
+  private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
+
+  protected static CallbackManager getCallbackManager() {
+    return mCallbackManager;
+  }
+
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
     public boolean getUseDeveloperSupport() {
@@ -25,6 +33,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new FBSDKPackage(mCallbackManager),
             new MapsPackage(),
             new VectorIconsPackage()
       );
